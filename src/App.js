193754e4af1b3223aa465675719dda1747306cc1
wwepopster <wwepopster1234567890@gmail.com>
2023-01-04 19:17:52 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div className={styles.category}>
+        <h1>Buttons</h1>
+        <button className={styles.acceptButton}>Add</button>
+        <button className={styles.cancleButton}>Cancle</button>
+      </div>
+      <div className={styles.category}>
+        <h1>Card</h1>
+        <div className={styles.card}>
+          <div className={styles.cardHeader}>
+            <p>Personal card</p>
+          </div>
+          <div className={styles.cardBody}>
+            <div>Name: Retardo</div>
+            <div>Position: Handyman</div>
+            <div>Age: 27 years</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
